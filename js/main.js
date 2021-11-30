f585711c3163400f392f888221211daa847f3747
Chirag Rastogi <crastogi2000@gmail.com>
2021-11-29 22:03:04 -0600

    Images needed

---
+++ js/main.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function($){
+	//set animation timing
+	var animationDelay = 2500;
+	
+	animateHeadline($('.cd-headline'));
+	
+	function animateHeadline($headlines) {
+		var duration = animationDelay;
+		$headlines.each(function(){
+			var headline = $(this);
+			//trigger animation
+			setTimeout(function(){ hideWord( headline.find('.is-visible').eq(0) ) }, duration);
+		});
+	}
+
+	function hideWord($word) {
+		var nextWord = takeNext($word);
+		switchWord($word, nextWord);
+		setTimeout(function(){ hideWord(nextWord) }, animationDelay);
+	}
+
+	function takeNext($word) {
+		return (!$word.is(':last-child')) ? $word.next() : $word.parent().children().eq(0);
+	}
+
+	function takePrev($word) {
+		return (!$word.is(':first-child')) ? $word.prev() : $word.parent().children().last();
+	}
+
+	function switchWord($oldWord, $newWord) {
+		$oldWord.removeClass('is-visible').addClass('is-hidden');
+		$newWord.removeClass('is-hidden').addClass('is-visible');
+	}
+});
