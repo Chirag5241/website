548a268bacd2dc707c3fdc12a6744acbe093a574
Chirag5241 <crastogi2000@gmail.com>
2025-03-25 00:24:10 -0700

    blog light mode fixed

--- js/navbar-side-collapse.js
+++ js/navbar-side-collapse.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const blogLink = document.getElementById("blog-page");
-    blogLink?.addEventListener("click", () => {
-      sessionStorage.setItem("triggerCollapse", "true");
-    });
+  const blogLink = document.getElementById("blog-page");
+  blogLink?.addEventListener("click", () => {
+    sessionStorage.setItem("triggerCollapse", "true");
   });
+});
